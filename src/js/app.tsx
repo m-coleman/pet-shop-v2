9e834179c161ea9d7881b7f64baa4a84ee9b21f5
Matt Coleman <m-coleman@users.noreply.github.com>
2022-01-21 00:57:59 -0800

    show some pets

--- src/js/app.tsx
+++ src/js/app.tsx
@@ -1,13 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Pet } from "./pets";
+import { allPets } from "./pets";
 
-ReactDOM.render(<App />, document.getElementById("react"));
+ReactDOM.render(<App/>, document.getElementById("react"));
 
 function App() {
-    let name: string = "cryptolord";
-    return (<div>Hello {name}</div>);
+
+    function getPetCards() {
+        return allPets.map((p: Pet, i: number) => {
+            return <PetCard pet={p} key={i}/>
+        });
+    }
+
+    return (
+        <div>
+            <h1 className="text-center">Cryptolord's Pet Shop</h1>
+            {getPetCards()}
+        </div>
+    );
 }
 
-function PetCard() {
-
+function PetCard(props: any) {
+    let p: Pet = props.pet;
+    return (
+        <div>
+            <div>{p.name}</div>
+            <img src={p.picture}></img>
+            <div>{p.id}</div>
+            <div>{p.age}</div>
+            <div>{p.breed}</div>
+            <div>{p.location}</div>
+            <br/>
+        </div>
+    );
 }
---
+++ src/js/pets.ts
@@ -0,0 +1,142 @@
+export interface Pet {
+  id: number,
+  name: string,
+  picture: string,
+  age: number,
+  breed: string,
+  location: string,
+  price?: number,
+  owner?: string,
+}
+
+export const allPets: Pet[] = [
+    {
+      "id": 0,
+      "name": "Frieda",
+      "picture": "resources/images/scottish-terrier.jpeg",
+      "age": 3,
+      "breed": "Scottish Terrier",
+      "location": "Lisco, Alabama"
+    },
+    {
+      "id": 1,
+      "name": "Gina",
+      "picture": "resources/images/scottish-terrier.jpeg",
+      "age": 3,
+      "breed": "Scottish Terrier",
+      "location": "Tooleville, West Virginia"
+    },
+    {
+      "id": 2,
+      "name": "Collins",
+      "picture": "resources/images/french-bulldog.jpeg",
+      "age": 2,
+      "breed": "French Bulldog",
+      "location": "Freeburn, Idaho"
+    },
+    {
+      "id": 3,
+      "name": "Melissa",
+      "picture": "resources/images/boxer.jpeg",
+      "age": 2,
+      "breed": "Boxer",
+      "location": "Camas, Pennsylvania"
+    },
+    {
+      "id": 4,
+      "name": "Jeanine",
+      "picture": "resources/images/french-bulldog.jpeg",
+      "age": 2,
+      "breed": "French Bulldog",
+      "location": "Gerber, South Dakota"
+    },
+    {
+      "id": 5,
+      "name": "Elvia",
+      "picture": "resources/images/french-bulldog.jpeg",
+      "age": 3,
+      "breed": "French Bulldog",
+      "location": "Innsbrook, Illinois"
+    },
+    {
+      "id": 6,
+      "name": "Latisha",
+      "picture": "resources/images/golden-retriever.jpeg",
+      "age": 3,
+      "breed": "Golden Retriever",
+      "location": "Soudan, Louisiana"
+    },
+    {
+      "id": 7,
+      "name": "Coleman",
+      "picture": "resources/images/golden-retriever.jpeg",
+      "age": 3,
+      "breed": "Golden Retriever",
+      "location": "Jacksonwald, Palau"
+    },
+    {
+      "id": 8,
+      "name": "Nichole",
+      "picture": "resources/images/french-bulldog.jpeg",
+      "age": 2,
+      "breed": "French Bulldog",
+      "location": "Honolulu, Hawaii"
+    },
+    {
+      "id": 9,
+      "name": "Fran",
+      "picture": "resources/images/boxer.jpeg",
+      "age": 3,
+      "breed": "Boxer",
+      "location": "Matheny, Utah"
+    },
+    {
+      "id": 10,
+      "name": "Leonor",
+      "picture": "resources/images/boxer.jpeg",
+      "age": 2,
+      "breed": "Boxer",
+      "location": "Tyhee, Indiana"
+    },
+    {
+      "id": 11,
+      "name": "Dean",
+      "picture": "resources/images/scottish-terrier.jpeg",
+      "age": 3,
+      "breed": "Scottish Terrier",
+      "location": "Windsor, Montana"
+    },
+    {
+      "id": 12,
+      "name": "Stevenson",
+      "picture": "resources/images/french-bulldog.jpeg",
+      "age": 3,
+      "breed": "French Bulldog",
+      "location": "Kingstowne, Nevada"
+    },
+    {
+      "id": 13,
+      "name": "Kristina",
+      "picture": "resources/images/golden-retriever.jpeg",
+      "age": 4,
+      "breed": "Golden Retriever",
+      "location": "Sultana, Massachusetts"
+    },
+    {
+      "id": 14,
+      "name": "Ethel",
+      "picture": "resources/images/golden-retriever.jpeg",
+      "age": 2,
+      "breed": "Golden Retriever",
+      "location": "Broadlands, Oregon"
+    },
+    {
+      "id": 15,
+      "name": "Terry",
+      "picture": "resources/images/golden-retriever.jpeg",
+      "age": 2,
+      "breed": "Golden Retriever",
+      "location": "Dawn, Wisconsin"
+    }
+  ]
+  
